1337eea24e5d919d3e51df47f8f2f510c6314547
spastorsegura <67179586+spastorsegura@users.noreply.github.com>
2021-05-19 01:26:42 -0500

    subir primera parte

--- app.js
+++ app.js
@@ -1,22 +1,99 @@
-var express = require('express');
-var exphbs  = require('express-handlebars');
-var port = process.env.PORT || 3000
+var express = require("express");
+var exphbs = require("express-handlebars");
+var mercadopago = require("mercadopago");
+require("dotenv").config();
+var port = process.env.PORT || 3000;
+mercadopago.configure({
+  access_token: process.env.ACCESS_TOKEN,
+  integrator_id: process.env.INTEGRATOR_ID,
+});
 
 var app = express();
- 
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
 
-app.use(express.static('assets'));
- 
-app.use('/assets', express.static(__dirname + '/assets'));
+const cliente = {
+  name: "Lalo",
+  surname: "Landa",
+  email: "test_user_46542185@testuser.com",
+  phone: {
+    number: 5549737300,
+    area_code: "52",
+  },
+  address: {
+    zip_code: "03940",
+    street_name: "Insurgentes Sur",
+    street_number: 1602,
+  },
+  identification: {
+    type: "DNI",
+    number: "223344455",
+  },
+};
 
-app.get('/', function (req, res) {
-    res.render('home');
+const metodos_pago = {
+  installments: 6,
+  excluded_payment_methods: [
+    {
+      id: "diners",
+    },
+  ],
+  exclude_payment_types: [
+    {
+      id: "atm",
+    },
+  ],
+};
+
+const preferencia = {
+  items: [],
+  back_urls: {
+    success: "",
+    pending: "",
+    failure: "",
+  },
+  payment_methods: metodos_pago,
+  payer: cliente,
+  auto_return: "approved",
+  notification_url: "",
+  external_reference: "sergio_cybert@hotmail.com",
+};
+
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+app.use(express.static("assets"));
+
+app.use("/assets", express.static(__dirname + "/assets"));
+
+app.get("/", function (req, res) {
+  res.render("home");
 });
 
-app.get('/detail', function (req, res) {
-    res.render('detail', req.query);
+app.get("/detail", async function (req, res) {
+  console.log(req.query);
+
+  const { img, title, price, unit } = req.query;
+  const item = {
+    id: "1234",
+    title: title,
+    description: "Dispositivo móvil de Tienda e-commerce",
+    picture_url: img,
+    quantity: +unit,
+    currency_id: "PEN",
+    unit_price: +price,
+  };
+  preferencia.items.push(item);
+  preferencia.back_urls.failure = `${req.get("host")}/failure`;
+  preferencia.back_urls.success = `${req.get("host")}/success`;
+  preferencia.back_urls.pending = `${req.get("host")}/pending`;
+
+  const respuesta = await mercadopago.preferences.create(preferencia);
+  console.log(respuesta);
+  res.render("detail", req.query);
 });
+
+app.get("/success",function(req,res){
+  console.log(req.query)
+  res.render("success",req.query)
+})
 
 app.listen(port);

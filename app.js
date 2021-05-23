65cdf63959c21ef16455efa90aebffcbea9f88ed
spastorsegura <67179586+spastorsegura@users.noreply.github.com>
2021-05-23 16:03:12 -0500

    agregue cors

--- app.js
+++ app.js
@@ -13,6 +13,17 @@ mercadopago.configure({
 var app = express();
 app.use(json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origins", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, Content-Type, Access-Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  next();
+});
+
+
 const cliente = {
   name: "Lalo",
   surname: "Landa",

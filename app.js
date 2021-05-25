40b4abc9b3c1055f66a6f052015577cbbfc19ecf
spastorsegura <67179586+spastorsegura@users.noreply.github.com>
2021-05-25 11:45:51 -0500

    modificacion picture_url

--- app.js
+++ app.js
@@ -95,11 +95,12 @@ app.get("/detail", async function (req, res) {
     currency_id: "PEN",
     unit_price: +price,
   };
+  console.log(item)
   preferencia.items.push(item);
   preferencia.back_urls.failure = `${req.get("host")}/failure`;
   preferencia.back_urls.success = `${req.get("host")}/success`;
   preferencia.back_urls.pending = `${req.get("host")}/pending`;
-  preferencia.notification_url = `${req.get("host")}/notificaciones`;
+  // preferencia.notification_url = `${req.get("host")}/notificaciones`;
 
   const respuesta = await mercadopago.preferences.create(preferencia);
   console.log(respuesta);
